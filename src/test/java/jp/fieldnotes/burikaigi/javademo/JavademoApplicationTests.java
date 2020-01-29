87976afbc361e772a0204e77685cee01c2e39182
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2020-01-30 00:22:45 +0900

    first commit

---
+++ src/test/java/jp/fieldnotes/burikaigi/javademo/JavademoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.fieldnotes.burikaigi.javademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
